bd5d5bc2d71cfd7e713f885b4ecc57401274bf1e
[Andrew Epermanis] <[aepermanis@gmail.com]>
2023-08-07 17:04:30 -0400

    added inquirer, wrote out first try at questions and put out template for readMe

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = 
+[
+    {
+        name: "Title",
+        message: "What is the title of your project?",
+        type: "input",
+    },
+    {
+        name: "Description",
+        message: "Describe the functionality and purpose of your project?",
+        type: "input",
+    },
+    {
+        name: "Installation",
+        message: "What steps are required to install your project?",
+        type: "input",
+    },
+    {
+        name: "Usage",
+        message: "How would a user go about interacting with your project?",
+        type: "input"
+    },
+    {
+        name: "Credits",
+        message: "Who helped contribute to this project?",
+        type: "input"
+    },
+    {
+        name: "License",
+        message: "What license is this project using?",
+        type: "input"
+    },
+    {
+        name: "Features",
+        message: "Describe the project features",
+        type: "input"
+    },
+    {
+        name: "Contribute",
+        message: "How can the Community add to your project?",
+        type: "input"
+    },
+    {
+        name: "Tests",
+        message: "What tests have been run and how do users run them?",
+        type: "input"
+    }
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers)=>{
+        writeToFile(answers.Title, answers);
+    })
+    
+    
+}
 
 // Function call to initialize app
 init();
